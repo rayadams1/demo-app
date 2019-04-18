4fdf168ba2df6430d54c62e6a4409da3fbb7b9f1
Snigdhadeep Moitra <snigdhadeep.moitra@milvik.se>
2019-04-18 18:08:34 +0530

    Add Demo React App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = <h1>Hello World</h1>;
+//Babel will convert this into React.createElement reason why we have to import React on top even though we will not use it directly in our code
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(element);
+//The application is automatically restarted whenever we save any changes also called as Hot Module Reloading
+
+ReactDOM.render(element, document.getElementById("root"));
